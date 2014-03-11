34862f656d9e11cdfc42f44aa80c57f32a72fc2a
jungfengpaulwang <paul.wang@ischool.com.tw>
2014-03-11 21:45:35 +0800

    no message

---
+++ ImportExport/ImportUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aspose.Cells;
+
+namespace Student_UserDefinedData.ImportExport
+{
+    class ImportUtil
+    {
+        /// <summary>
+        /// 取得欄位驗證字串
+        /// </summary>
+        /// <returns></returns>
+        public static string GetChekcDataStr(int idx, Worksheet wst, Dictionary<string, int> ColIndexDic)
+        {
+            string chkStr = string.Empty;
+            if (ColIndexDic.ContainsKey("學號"))
+                chkStr += wst.Cells[idx, ColIndexDic["學號"]].StringValue;
+
+            if (ColIndexDic.ContainsKey("姓名"))
+                chkStr += wst.Cells[idx, ColIndexDic["姓名"]].StringValue;
+
+            if (ColIndexDic.ContainsKey("欄位名稱"))
+                chkStr += wst.Cells[idx, ColIndexDic["欄位名稱"]].StringValue;
+
+            return chkStr;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FISCA;
+
+namespace Student_UserDefinedData
+{
+    /// <summary>
+    /// 自訂資料欄位
+    /// </summary>
+    public class Program
+    {
+        [MainMethod("Student_UserDefineData")]
+        public static void Main()
+        {
+            LoadManager.Start();
+        }
+    }
+}
